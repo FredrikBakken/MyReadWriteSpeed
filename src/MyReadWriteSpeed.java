4c45887f91139fe6e275baaef2e2e41071a29059
Fredrik Bakken <fredda10x@gmail.com>
2016-10-13 17:35:46 +0200

    Minor changes
    Minor changes to the tool has been done and a new release has been
    published to the releases folder.

--- src/MyReadWriteSpeed.java
+++ src/MyReadWriteSpeed.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
